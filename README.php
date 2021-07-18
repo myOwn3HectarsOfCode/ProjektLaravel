9604de5ad98453d8bf19588ec1a1947de2af839c
myOWn3HectarsOfCode <leczyckaola6@gmail.com>
2021-07-18 20:19:56 +0200

    PHP commit

---
+++ README.php
@@ -0,0 +1,111 @@
+<?php<!DOCTYPE HTML>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<title></title>
+</head>
+<body>
+
+
+<p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400"></a></p>
+<p align="center">
+<button type="button"
+ style="background-color:#c5d1e0;
+ color:white;
+border-top-left-radius: 5px 5px;
+border-top-right-radius: 5px 5px;
+border-bottom-left-radius: 5px 5px;
+border-bottom-right-radius: 5px 5px;
+a:hover color: white">
+<h1><a href="#o moim projekcie">My ProjektLaravel</a></h1>	
+</button>
+</p>
+<p align="center">
+<a href="https://travis-ci.org/laravel/framework"><img src="https://travis-ci.org/laravel/framework.svg" alt="Build Status"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/dt/laravel/framework" alt="Total Downloads"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/v/laravel/framework" alt="Latest Stable Version"></a>
+<a href="https://packagist.org/packages/laravel/framework"><img src="https://img.shields.io/packagist/l/laravel/framework" alt="License"></a>
+</p>
+
+## About Laravel</p>
+
+<br>Laravel is a web application framework with expressive, elegant syntax. We believe development must be an enjoyable and creative experience to be truly fulfilling. Laravel takes the pain out of development by easing common tasks used in many web projects, such as:
+
+<ul>- [Simple, fast routing engine](https://laravel.com/docs/routing).</ul>
+<ul>- [Powerful dependency injection container](https://laravel.com/docs/container).</ul>
+<ul>- Multiple back-ends for [session](https://laravel.com/docs/session) and [cache](https://laravel.com/docs/cache) storage.</ul>
+<ul>- Expressive, intuitive [database ORM](https://laravel.com/docs/eloquent).</ul>
+<ul>- Database agnostic [schema migrations](https://laravel.com/docs/migrations).</ul>
+<ul>- [Robust background job processing](https://laravel.com/docs/queues).</ul>
+<ul>- [Real-time event broadcasting](https://laravel.com/docs/broadcasting).</ul>
+
+
+Laravel is accessible, powerful, and provides tools required for large, robust applications.</p>
+
+<br>## Learning Laravel
+
+<br>Laravel has the most extensive and thorough [documentation](https://laravel.com/docs) and video tutorial library of all modern web application frameworks, making it a breeze to get started with the framework.</br>
+
+If you don't feel like reading, [Laracasts](https://laracasts.com) can help. Laracasts contains over 1500 video tutorials on a range of topics including Laravel, modern PHP, unit testing, and JavaScript. Boost your skills by digging into our comprehensive video library.
+
+<p>## Laravel Sponsors</p>
+
+<br>We would like to extend our thanks to the following sponsors for funding Laravel development. If you are interested in becoming a sponsor, please visit the Laravel [Patreon page](https://patreon.com/taylorotwell).
+
+<br>### Premium Partners
+
+<ul>- **[Vehikl](https://vehikl.com/)** </ul>
+<ul>- **[Tighten Co.](https://tighten.co)**l </ul>
+<ul>- **[Kirschbaum Development Group](https://kirschbaumdevelopment.com)**</ul>
+<ul>- **[64 Robots](https://64robots.com)**</ul>
+<ul>- **[Cubet Techno Labs](https://cubettech.com)**</ul>
+<ul>- **[Cyber-Duck](https://cyber-duck.co.uk)**</ul>
+<ul>- **[Many](https://www.many.co.uk)**</ul>
+<ul>- **[Webdock, Fast VPS Hosting](https://www.webdock.io/en)**</ul>
+<ul>- **[DevSquad](https://devsquad.com)**</ul>
+<ul>- **[Curotec](https://www.curotec.com/services/technologies/laravel/)**</ul>
+<ul>- **[OP.GG](https://op.gg)**</ul>
+
+## Contributing
+
+</br>Thank you for considering contributing to the Laravel framework! The contribution guide can be found in the [Laravel documentation](https://laravel.com/docs/contributions).
+
+</br>## Code of Conduct
+
+</br>In order to ensure that the Laravel community is welcoming to all, please review and abide by the [Code of Conduct](https://laravel.com/docs/contributions#code-of-conduct).
+
+</br>## Security Vulnerabilities
+
+</br>If you discover a security vulnerability within Laravel, please send an e-mail to Taylor Otwell via [taylor@laravel.com](mailto:taylor@laravel.com). All security vulnerabilities will be promptly addressed.
+
+</br>## License
+
+</br>The Laravel framework is open-sourced software licensed under the [MIT license](https://opensource.org/licenses/MIT).<hr>
+<p><button type="button"
+ style="background-color:#c5d1e0;
+ color:white;
+border-top-left-radius: 5px 5px;
+border-top-right-radius: 5px 5px;
+border-bottom-left-radius: 5px 5px;
+border-bottom-right-radius: 5px 5px;
+a:hover color: white">
+<h1><a name="o moim projekcie">My ProjektLaravel</a></h1>	
+</button>
+</p>
+<p>Project to learning Laravel (and PHP).<br>
+It will hand-held(with filtering of data to displaing) generator of reports from excel file (user must-by the use of a browser e.g. in the Intranet- to regiser, log in and than enters his .xls file;<br>
+  Next he chooses data for visualisation from columns of excel and the application draws charts. When he logging out his data will removed from the database).<br>
+Application must to have architecture according to MVC pattern and must to provide   data security of user.<br>
+At the beginning  was making: <br>Installing laravel-framework and additional components (Composer);<br> checking php version,<br> checking enviroment path in WindowsSystem, <br>
+installing composer and laravel/ui pack (there steps are described in Laravel documentacion for 7x version).<br><br>
+<br>
+My idea is to  download bootstrap theme with ready charts and to changing its according to this plan( as agreed - quenes of littler tasks in the project)<br>
+My ProjectLaravel:<br>
+-php version 8.0<br>
+-Laravel version 8.41.1<br>
+-Composer version 2.1.3<br>
+-laravel/ui 3.3<br>
+-npm version 6.14.13</p></a>
+</body>
+</html>
+?>
