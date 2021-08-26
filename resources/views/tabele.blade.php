13fc873508e0d806cf809545d0e796e2931016e3
myOWn3HectarsOfCode <leczyckaola6@gmail.com>
2021-08-26 20:33:32 +0200

    uwzględnienie asset dla popraw css #7

--- resources/views/tabele.blade.php
+++ resources/views/tabele.blade.php
@@ -5,10 +5,9 @@
  <div class="container-fluid">
 
 <!-- Page Heading -->
-<h1 class="h3 mb-2 text-gray-800">Tables</h1>
-<p class="mb-4">DataTables is a third party plugin that is used to generate the demo table below.
-    For more information about DataTables, please visit the <a target="_blank"
-        href="https://datatables.net">official DataTables documentation</a>.</p>
+<h1 class="h3 mb-2 text-gray-800">Tabele</h1>
+<p class="mb-4">Dane importowanej tabeli mogą pojawić się właśnie na tej stronie.</br>Oto przykład tabeli dostarczonej w szablonie Bootstrapa<a target="_blank"
+        href="https://datatables.net">&nbsp (oficjalna dokumentacja kodu - tutaj- DataTables documentation</a>.</p>
 
 <!-- DataTales Example -->
 <div class="card shadow mb-4">
